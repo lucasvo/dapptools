c1ea70921f79be9752f0141377785d9f3e586992
Martin Lundfall <martin.lundfall@protonmail.com>
2020-04-06 16:53:03 +0200

    typo fix

--- src/hevm/hevm-cli/hevm-cli.hs
+++ src/hevm/hevm-cli/hevm-cli.hs
@@ -316,7 +316,7 @@ dappTest opts _ solcFile = do
         error ("Failed to read Solidity JSON for `" ++ solcFile ++ "'")
 
 regexMatches :: Text -> Text -> Bool
-  regexMatches regexSource =
+regexMatches regexSource =
   let
     compOpts =
       Regex.defaultCompOpt { Regex.lastStarGreedy = True }
